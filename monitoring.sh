7f83be780eff33a422b07c3bebb5b24c4403677e
Haruka Omura <homura@c6r3s13.42tokyo.jp>
2025-07-11 19:17:49 +0900

    edit

--- monitoring.sh
+++ monitoring.sh
@@ -12,10 +12,10 @@ v_cpu=$(grep "processor" /proc/cpuinfo | wc -l)
 # RAM
 ram_total=$(free --mega | awk '$1 == "Mem:" {print $2}')
 ram_used=$(free --mega | awk '$1 == "Mem:" {print $3}')
-ram_rate=$(free --mega | awk '$1 == "Mem:" {print $3/$2 * 100}')
+ram_rate=$(free --mega | awk '$1 == "Mem:" {printf("%.2f", $3/$2 * 100)}')
 
 # DISK
-disk_total=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_t += $2} END {printf ("%.1f\n"), disk_t/1024}')
+disk_total=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_t += $2} END {printf "%.1f", disk_t/1024}')
 disk_used=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_u += $3} END {print disk_u}')
 disk_rate=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_u += $3} {disk_t+= $2} END {printf("%d"), disk_u/disk_t*100}')
 
@@ -31,7 +31,7 @@ last_boot=$(who -b | awk '$1 == "system" {print $3 " " $4}')
 use_lvm=$(lsblk | grep -q "lvm" && echo yes || echo no)
 
 # TCP CONNECTIONS
-cpu_connection=$(ss -ta | grep "ESTAB" | wc -l)
+tcp_connection=$(ss -ta | grep "ESTAB" | wc -l)
 
 # USER COUNT
 user_count=$(users | wc -w)
@@ -41,17 +41,17 @@ ip=$(hostname -I)
 mac=$(ip link | grep "link/ether" | awk '{print $2}')
 
 # SUDO
-sudo=$(journalctl _COMM=sudo| grep COMMAND)
+sudo=$(journalctl _COMM=sudo| grep COMMAND | wc -l)
 
 printf "#Architecture: $arch\n";
 printf "#CPU physical: $p_cpu\n";
 printf "#vCPU: $v_cpu\n";
 printf "#Memory Usage: $ram_used/${ram_total}MB (${ram_rate}%%)\n";
 printf "#Disk Usage: $disk_used/${disk_total}Gb (${disk_rate}%%)\n";
-printf "#CPU load: $cpu_fin%%\n";
+printf "#CPU load: $cpu_usage_formatted%%\n";
 printf "#Last boot: $last_boot\n";
 printf "#LVM use: $use_lvm\n";
-printf "#Connections TCP: $cpu_connection\n";
+printf "#Connections TCP: $tcp_connection ESTABLISHED\n";
 printf "#User log: $user_count\n";
 printf "#Network: IP $ip ($mac)\n";
 printf "#Sudo: $sudo cmd\n";
